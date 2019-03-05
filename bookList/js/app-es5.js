646d8963d3783b3f1172da879f44ce36287c4037
knromaric <nzekeng@gmail.com>
2019-03-06 00:35:06 +0200

    bookList: Add book feature + alert message

--- bookList/js/app-es5.js
+++ bookList/js/app-es5.js
@@ -1,3 +1,61 @@
 //book Constructor
-
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
 //UI Constructor
+function UI() {}
+
+UI.prototype.showAlert = function (message, className){
+  const div = document.createElement('div');
+  div.className = `${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector('.container');
+  const form = document.getElementById('book-form');
+  container.insertBefore(div, form);
+  setTimeout(function(){
+    div.style.display = 'none';
+
+  }, 3000);
+}
+
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  list.appendChild(row);
+}
+
+//Event listeners 
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title, author, isbn);
+  const ui = new UI();
+
+  //validate
+  if (title === '' || author === '' || isbn === '') {
+    //error
+    ui.showAlert('please fill in all fields', 'error');
+  } else {
+    ui.addBookToList(book);
+    ui.clearFields();
+    ui.showAlert('Book Added Succesfully', 'success');
+  }
+});
