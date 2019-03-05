ad4051d9ed09847790a5849a6803712dd30454ce
knromaric <nzekeng@gmail.com>
2019-03-05 13:23:52 +0200

    numberGuesser: added Html UI

---
+++ numberGuesser/app.js
@@ -0,0 +1,8 @@
+/**
+ * game function
+ * -player must guess a number between a min and max
+ * - player gets a certain amount of guesses
+ * - Notify player of guesses remaining
+ * - Notify Player of the correct answer if loose
+ * - Let Player choose to play again
+ */
