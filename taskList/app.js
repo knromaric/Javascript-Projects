2e33516262a3d2f05c5a9e12a3e01ed31feb40c9
knromaric <nzekeng@gmail.com>
2019-03-05 11:38:20 +0200

    Persisted to LocalStorage

--- taskList/app.js
+++ taskList/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 //load all event listeners
 function loadEventListeners() {
+  //DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks)
   //add task event
   form.addEventListener('submit', addTask);
   //remove task event
@@ -21,22 +23,41 @@ function loadEventListeners() {
 
 }
 
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(task => {
+    createSingleTaskElement(task);
+  });
+}
+
 function filterTasks(e) {
   const text = e.target.value.toLowerCase();
-  document.querySelectorAll('.collection-item').forEach(function(task){
+  document.querySelectorAll('.collection-item').forEach(function (task) {
     const item = task.firstChild.textContent;
-    if(item.toLowerCase().indexOf(text) !== -1){
+    if (item.toLowerCase().indexOf(text) !== -1) {
       task.style.display = 'block';
-    }else{
+    } else {
       task.style.display = 'none';
     }
   });
 }
 
 function clearAllTasks() {
-  while(taskList.firstChild) {
+  while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  clearAllTasksFromLS();
+}
+
+function clearAllTasksFromLS() {
+  localStorage.clear();
 }
 
 function removeTask(e) {
@@ -44,8 +65,29 @@ function removeTask(e) {
     if (confirm('Are You Sure ?')) {
       const li = e.target.parentElement.parentElement;
       li.remove();
+      removeTaskFromLocalStorage(li);
     }
   }
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task, index) => {
+    if (taskItem.textContent === task) {
+      console.log('taskitem: ', taskItem);
+      console.log('task: ', task);
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
 }
 
 function addTask(e) {
@@ -54,14 +96,32 @@ function addTask(e) {
   if (taskInput.value === '') {
     alert('add a task');
   }
+  createSingleTaskElement(taskInput.value);
+  //store in LS
+  storeTaskInLocalStorage(taskInput.value);
+  taskInput.value = '';
+}
+
+function createSingleTaskElement(text) {
   const li = document.createElement('li');
   li.className = 'collection-item';
-  li.appendChild(document.createTextNode(taskInput.value));
+  li.appendChild(document.createTextNode(text));
   const link = document.createElement('a');
   link.className = 'delete-item secondary-content';
   link.innerHTML = '<i class="fas fa-times"></i>';
   link.style.cursor = 'pointer';
   li.appendChild(link);
   taskList.appendChild(li);
-  taskInput.value = '';
+}
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
