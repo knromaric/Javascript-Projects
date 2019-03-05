d2cfb85c675527177e643d85e02bc6a5ac4024a9
knromaric <nzekeng@gmail.com>
2019-03-05 11:12:01 +0200

    Add the filter feature

--- taskList/app.js
+++ taskList/app.js
@@ -16,9 +16,24 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   //clear all tasks
   clearBtn.addEventListener('click', clearAllTasks);
+  //filter tasks
+  filter.addEventListener('keyup', filterTasks);
+
 }
 
-function clearAllTasks(e) {
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) !== -1){
+      task.style.display = 'block';
+    }else{
+      task.style.display = 'none';
+    }
+  });
+}
+
+function clearAllTasks() {
   while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
