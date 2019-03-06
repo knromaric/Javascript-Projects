4c6e14c1b1aaa219409d1c1ccee603a93e05f2f9
knromaric <nzekeng@gmail.com>
2019-03-06 12:17:59 +0200

    easyHttp: Added put and delete methods

--- easyHTTP/js/app.js
+++ easyHTTP/js/app.js
@@ -1,13 +1,51 @@
 const http = new EasyHTTP();
 
 //get posts
-http.get('https://jsonplaceholder.typicode.com/posts', loadPosts);
+// http.get('https://jsonplaceholder.typicode.com/posts', loadPosts);
 
-function loadPosts(error, response){
-  if(error){
+// function loadPosts(error, response){
+//   if(error){
+//     console.log(error);
+//   }else{
+//     console.log(response);
+//   }
+// }
+
+//POST 
+const data = {
+  title: 'roma in the building',
+  body: 'This is my year'
+}
+// http.post('https://jsonplaceholder.typicode.com/posts', data,  addPost)
+
+// function addPost(error, response){
+//   if(error){
+//     console.log(error);
+//   } else {
+//     console.log('response: ', response);
+//   }
+// }
+
+//PUT
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, updatePost);
+
+// function updatePost(error, response) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('response: ', response);
+//   }
+// }
+
+//DELETE
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', deletePost);
+
+function deletePost(error, response) {
+  if (error) {
     console.log(error);
-  }else{
-    console.log(response);
+  } else {
+    console.log('POST DELETED');
   }
-  
 }
--- easyHTTP/js/easyhttp.js
+++ easyHTTP/js/easyhttp.js
@@ -6,16 +6,54 @@
  EasyHTTP.prototype.get = function (url, callback) {
    this.http.open('GET', url, true);
    this.http.onload = function () {
-      if(this.http.status === 200){
-        callback(null, this.http.responseText) ;
-      }else{
-        callback(`Error: ${this.http.status}`);
-      }
+     if (this.http.status === 200) {
+       callback(null, this.http.responseText);
+     } else {
+       callback(`Error: ${this.http.status}`);
+     }
    }.bind(this);
 
    this.http.send();
  }
 
  //Make an http POST Request
+ EasyHTTP.prototype.post = function (url, data, callback) {
+   this.http.open('POST', url, true);
+   this.http.setRequestHeader('content-type', 'application/json');
+
+   this.http.onload = function () {
+     callback(null, this.http.responseText);
+   }.bind(this);
+
+   this.http.send(JSON.stringify(data));
+ }
+
+
  //Make an http PUT Request
+
+ EasyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('content-type', 'application/json');
+
+  this.http.onload = function () {
+    callback(null, this.http.responseText);
+  }.bind(this);
+
+  this.http.send(JSON.stringify(data));
+}
+
+
  //Make an http DELETE Request
+
+ EasyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+  this.http.onload = function () {
+    if (this.http.status === 200) {
+      callback(null, this.http.responseText);
+    } else {
+      callback(`Error: ${this.http.status}`);
+    }
+  }.bind(this);
+
+  this.http.send();
+}
