7d2b28d904c649bdaab72b7d7ea02c27087d71f3
knromaric <nzekeng@gmail.com>
2019-03-06 14:14:13 +0200

    easyHTTP2: using fetch API

---
+++ easyHTTP2/js/app.js
@@ -0,0 +1,28 @@
+const http = new EasyHTTP();
+
+//Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+//user data
+const data = {
+  name: 'rom ze', 
+  username: 'roze',
+  email: 'rze@gmail.com'
+}
+
+//create post
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+//create put
+// http.put('https://jsonplaceholder.typicode.com/users/1', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+//create DELETE
+http.delete('https://jsonplaceholder.typicode.com/users/1')
+  .then((data) => console.log(data))
+  .catch(err => console.log(err));
