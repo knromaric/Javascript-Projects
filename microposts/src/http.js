dd88b170543102523068d3cbb906ce268654e6ee
knromaric <nzekeng@gmail.com>
2019-03-14 04:38:42 +0200

    microposts: showPosts feature Added

---
+++ microposts/src/http.js
@@ -0,0 +1,54 @@
+/**
+ * EasyHTTP Library
+ * Library for making http request
+ * 
+ * @version 2.0.0
+ * @author Rom
+ * @licence de-kod
+ *  
+ */
+
+class EasyHTTP {
+  //make HTTP GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  //make HTTP POST request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = response.json();
+    return resData;
+  }
+
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE'
+    })
+    const resData = await "Resource deleted";
+    return resData;
+
+  }
+}
+
+export const http = new EasyHTTP();
