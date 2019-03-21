aeb0bf5c4f5b857d867e3fd54bb2c7cc8a7bed0d
knromaric <nzekeng@gmail.com>
2019-03-21 12:21:45 +0200

    pdf-viewer: display and browse feature

---
+++ PDF-viewer/js/main.js
@@ -0,0 +1,78 @@
+const url = '../docs/photography.pdf';
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.8,
+      canvas = document.querySelector("#pdf-render"),
+      ctx = canvas.getContext('2d');
+
+//render the page
+const renderPage = num => {
+  pageIsRendering = true;
+
+  //get Page
+  pdfDoc.getPage(num).then(page=>{
+    //Set scale
+    const viewport = page.getViewport({scale});
+    canvas.height =viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext : ctx,
+      viewport
+    }
+
+    page.render(renderCtx).promise.then(()=>{
+      pageIsRendering = false;
+
+      if(pageNumIsPending !== null){
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+    //Output the current page
+    document.querySelector('#page-num').textContent = num;
+  })
+}
+
+//Check for pages rendering
+const queueRenderPage = num =>{
+  if(pageIsRendering) {
+    pageNumIsPending = num;
+  }
+  else{
+    renderPage(num);
+  }
+}
+
+//Show prev Page
+const showPrevPage = ()=>{
+  if(pageNum <= 1){
+    return;
+  }
+  pageNum --;
+  queueRenderPage(pageNum);
+}
+
+//Show next Page
+const showNextPage = ()=>{
+  if(pageNum >= pdfDoc.numPages){
+    return;
+  }
+  pageNum ++;
+  queueRenderPage(pageNum);
+}
+
+// Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_=>{
+  pdfDoc = pdfDoc_;
+  
+  document.querySelector("#page-count").textContent = pdfDoc.numPages;
+  renderPage(pageNum);
+});
+
+//button events
+document.querySelector("#prev-page").addEventListener('click', showPrevPage);
+document.querySelector("#next-page").addEventListener('click', showNextPage);
